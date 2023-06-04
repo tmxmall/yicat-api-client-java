d6a8012ffee110672f31f61a5649b81074c7a963
chenjian <chenj062521@126.com>
2023-06-04 10:45:14 +0800

    feat：更新

--- src/main/java/vip/yicat/client/QuickTranslateDemo.java
+++ src/main/java/vip/yicat/client/QuickTranslateDemo.java
@@ -14,7 +14,7 @@ public class QuickTranslateDemo {
 
     public static void main(String[] args) throws InterruptedException {
         // 请先生成token，并设置baseUrl，baseUrl为YiCAT服务的地址
-        Credentials credentials = new Credentials("fc6a127932ac401fbe7534a11cfdf54c", "http://192.168.60.186");
+        Credentials credentials = new Credentials("fc6a127932ac411fbe7534a11cfdf54c", "http://192.168.1.186");
         Client client = new Client(credentials);
         // 项目名称可能根据情况自己取名字
         String projectName = "project01";
@@ -22,7 +22,7 @@ public class QuickTranslateDemo {
         // 快速翻译，只传一个目标语言
         List<String> tgtLanList = Arrays.asList("en-US");
         String mtProvider = "TranSmart";
-        File file = new File("C:\\Users\\ChenJian\\Desktop\\misc\\对齐\\nature1-zh-少1.doc");
+        File file = new File("C:\\Users\\Desktop\\nature1-zh-少1.doc");
         // 获取所有的记忆库和术语库id，用于挂载，也可以不挂载。注意语言方向要匹配
         List<String> tmIds = client.getTranslationMemoryApi().listTMInfo().
                 stream().map(ApiYiCATTMInfo::getTmId).collect(Collectors.toList());
